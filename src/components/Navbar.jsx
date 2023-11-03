c89d33a86d9f423ee061334c19de77afc3a90e83
Luca Améaume <62576432+LuluxClient@users.noreply.github.com>
2023-11-03 15:39:18 +0100

    Add user info to Feed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,12 @@
-// import { useAtom } from "jotai";
 import { Link } from "react-router-dom";
-// import { userAtom } from "../utils/globalState";
 
 const Navbar = () => {
-//   const [user] = useAtom(userAtom);
 
   return (
     <nav>
       <Link to={"/"}>Déconnecter</Link>
       {" - "}
       <Link to={"/post"}>Poster</Link>
-      {" - "}
-      {/* <span>
-        Salut {user.name}
-      </span> */}
     </nav>
   );
 };
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useAtom } from "jotai";
+import { userAtom } from "../utils/globalState";
 
 const Feed = () => {
+    const [user] = useAtom(userAtom);
     return (
+        
         <div>
             <Navbar />
+            <h1>Bienvenue sur Instacrame {user.name}</h1>
         </div>
     );
 };
